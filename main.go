627b90791c45d997757f7d56fb319d761b063afb
asami <asami@admnoiMac.local>
2018-04-05 11:51:13 +0900

    [add] enable args

--- main.go
+++ main.go
@@ -11,10 +11,16 @@ import (
 )
 
 func main() {
-	// 標準入力から読み込み
-	stdin := bufio.NewScanner(os.Stdin)
-	stdin.Scan()
-	s := stdin.Text()
+	a := os.Args
+	s := "/"
+	if len(a) >= 2 {
+		s = os.Args[1]
+	} else {
+		// 標準入力から読み込み
+		stdin := bufio.NewScanner(os.Stdin)
+		stdin.Scan()
+		s = stdin.Text()
+	}
 	// 絶対パスへの変換
 	p, err := filepath.Abs(s)
 	if err != nil {
